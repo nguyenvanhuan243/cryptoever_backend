ec2ca3459c4d3daf6e23345bb81d38d16395654a
Huan Nguyen <nguyenvanhuan243@gmail.com>
2025-02-07 11:19:38 +0700

    add author github

--- controllers/healthController.js
+++ controllers/healthController.js
@@ -2,12 +2,14 @@ const sendError = require('./assets/errorController');
 
 exports.healthCheck = async (req, res) => {
   try {
+    const githubAuthor = 'https://github.com/nguyenvanhuan243';
     res.status(200).json({
       status: 'success',
       data: {
         env: process.env.NODE_ENV,
         test_env: process.env.TEST_ENV,
         message: 'Server is running',
+        author: githubAuthor,
         email: {
             port: process.env.GMAIL_PORT,
             host: process.env.HOST
